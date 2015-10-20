477a1ca668bb2a04642af6a7d968ed1b22296c11
saphron <saphron.scul@gmail.com>
2015-10-20 09:32:47 -0400

    challenge response

--- challenge.rb
+++ challenge.rb
@@ -1,46 +1,49 @@
 # Assessment :: Ruby Basics
 
-
 # Question 1: Ruby objects and classes.
 # How would one find the class of the below objects.
-
-"Hello World"
-33
-65.89
+"Hello World".class
+33.class
+65.89.class
 
 # Question 2: Booleans
 # What would be the result of executing the below statements
-
 if 33 && 55
   puts "it's true"
 else
   puts "it's false"
 end
 
+"it's true"
 # 2.1 Substitue "hello" && 4.5 for 33 && 55 above. What's the output?
+"it's true"
 # 2.2 Substitue nil && 88 for 33 && 55 above. What's the output?
+"it's false"
 # 2.4 Substitue 63.4 < 64. What's the output?
+"it's true"
 
 # Question 3 :Strings
-
 # How would you (Yes, you can look up the String methods!):
-
 # 3.1. determine if a string is included another another string?
 # 3.2. determine if the length of a string?
-# 3.3. Go from the string "eels in my hovercraft" to "fish in my sink" but don't
-# change the string "eels in my hovercraft"
-
+"String".length
+# 3.3. Go from the string "eels in my hovercraft" to "fish in my sink" but don't change the string "eels in my hovercraft"
+string = "eels in my hovercraft"
+strTwo = string.gsub!(/eels/, 'fish')
+strThree = strTwo.gsub!(/hovercraft/, 'sink')
+puts strThree
+puts string
 
 # Question 4 : Variables
-# Change the below string, do NOT make copies of the string. All changes
-# to the string should be made in-place. (Unless told otherwise, like in 4.3.)
-
+# Change the below string, do NOT make copies of the string. All changes to the string should be made in-place. (Unless told otherwise, like in 4.3.)
 vehicle = "4 wheel Bus"
-
 # 4.1 to "6 wheel Bus"
+vehicleTwo = vehicle.gsub!(/4/, '6')
 # 4.2 to "6 wheel tank"
+vehicleThree = vehicleTwo.gsub!(/Bus/, 'tank')
 # 4.3 Create a NEW string from the string in 4.2 that will be
 # "6 wheel tank that shoots rainbow bunnnies"
+puts "#{vehicleThree} that shoots rainbow bunnies"
 
 # Question 5 : Loop
 # - write a loop that guesses your age.
@@ -48,11 +51,31 @@ vehicle = "4 wheel Bus"
 # - Notify the user if they guess correctly OR if they run out of turns to guess
 # - Get mad and curse the fingers that thought you where that old,
 # - aaaah maaan c'mon, huh, WTF
-# - I'm not kidding, say unkind works to a user that thinks your 10 years older
-#   than you are.
+# - I'm not kidding, say unkind works to a user that thinks your 10 years older than you are.
+num_to_guess = 29
+max_tries = 4
+tries = 0
+
+while true
+  print "Guess Allie's age: "
+  guess = gets.chomp!.to_i
+  puts "You guessed #{guess}\n"
+
+  if guess >= (num_to_guess + 10)
+    puts "You think I'm that old?!? I'm way younger than that."
+  elsif guess == num_to_guess
+    puts "You're right, I am #{num_to_guess}years old."
+    break
+  end
+
+  tries += 1
+  if tries >= max_tries
+    puts "No more guesses."
+    break
+  end
+end
 
 # Bonus Question: Who said this?
 
-# The great enemy of the truth is very often not the lie -- deliberate, contrived
-# and dishonest, but the myth, persistent, persuasive, and unrealistic.
-# Belief in myths allows the comfort of opinion without the discomfort of thought.
+# The great enemy of the truth is very often not the lie -- deliberate, contrived and dishonest, but the myth, persistent, persuasive, and unrealistic.  Belief in myths allows the comfort of opinion without the discomfort of thought.
+John F. Kennedy
